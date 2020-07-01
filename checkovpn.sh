7d1525cb555826e6746e1089a577dcf37fcd8cf5
yaggen <54915130+yaggen@users.noreply.github.com>
2020-07-01 22:41:31 +0200

    Update checkovpn.sh
    Fixed logical errors making ovpn never run instead.

--- checkovpn.sh
+++ checkovpn.sh
@@ -1,11 +1,24 @@
+
+
 #!/bin/bash
 LOG="/var/log/ovpn.log"
 DATE=`date "+%Y-%m-%d %H:%M:%S"`
 
-
 #Kollar med ping om OVPN har anslutning, startar annars om.
-checkcmd=`ps aux | grep "openvpn --config" | grep -v "grep" | awk '{print $2}'`
+checkcmd=`pgrep openvpn`
 pubip=`curl https://www.ovpn.com/v2/api/client/ptr 2>/dev/null | cut -d '"' -f 6| awk '{print $1}'`
+
+if [ $pubip !=  "ValidationException" ]; then
+        echo "OVPN is up and running :)"
+else
+        echo "[$DATE] Restarting daemon" >> $LOG
+        kill -3 $checkcmd 2>/dev/null
+        sleep 2
+        openvpn --config /etc/openvpn/ovpn.conf --daemon
+        sleep 3
+        exec $0
+fi
+
 
 ping -c 1 8.8.8.8 &>/dev/null
 STATUS=$?
@@ -15,12 +28,8 @@ if [ $STATUS -ne 0 ]; then
     sleep 2
     echo "[$DATE] Service stopped, restarting OVPN" >> $LOG
     openvpn --config /etc/openvpn/ovpn.conf --daemon
-    echo "[$DATE] Checking public IP" >> $LOG
-    if [ $pubip != "MY-IP-HERE" ]; then
-        exec $0
-    else
-        echo "[$DATE] Connection reestablished, quitting" >> $LOG
-    fi
-else
-    echo "OVPN up and running :)"
 fi
+
+
+
+
